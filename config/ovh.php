49b7420a740d6c6f85f4aa135c395463e072a338
Chong Jin Yi <chong601.13@gmail.com>
2022-05-22 21:30:36 +0800

    Map configs from .env file to Laravel config

---
+++ config/ovh.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'application_key' => env('OVH_APPLICATION_KEY'),
+    'application_secret' => env('OVH_APPLICATION_SECRET'),
+    'consumer_key' => env('OVH_CONSUMER_KEY')
+];
