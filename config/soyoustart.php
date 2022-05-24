aca6563c0672bc8c3fd79aabe2a94a12ee4479d9
Chong Jin Yi <chong601.13@gmail.com>
2022-05-25 06:26:58 +0800

    Add SoYouStart config file

---
+++ config/soyoustart.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'application_key' => env('SOYOUSTART_APPLICATION_KEY'),
+    'application_secret' => env('SOYOUSTART_APPLICATION_SECRET'),
+    'consumer_key' => env('SOYOUSTART_CONSUMER_KEY')
+];
