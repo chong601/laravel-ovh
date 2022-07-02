4ce9e66e9fd7977dbfe377ab118ec4426a1a4011
Chong Jin Yi <chong601.13@gmail.com>
2022-07-02 10:25:13 +0800

    Add OVHcloud list all dedicated servers command

---
+++ app/Console/Commands/OvhCloudListAllDedicatedServers.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Services\OvhCloud\OvhCloudService;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\App;
+
+class OvhCloudListAllDedicatedServers extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'ovhcloud:listalldedicatedservers';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'List all subscribed OVHcloud dedicated servers';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        /** @var \Ovh\Api */
+        $ovh_api = App::makeWith(OvhCloudService::class, [
+            'application_key' => config('ovh.application_key'),
+            'application_secret' => config('ovh.application_secret'),
+            'endpoint' => 'ovh-ca',
+            'consumer_key' => config('ovh.consumer_key')
+        ]);
+
+        $results = $ovh_api->get('/dedicated/server');
+
+        print(json_encode($results, JSON_PRETTY_PRINT));
+
+        return 0;
+    }
+}
