b72ffcbbe5cf1da26ba7899c4bcee86989d0aa7b
Chong Jin Yi <chong601.13@gmail.com>
2022-07-20 17:45:37 +0800

    Complete base IP address implementation

--- app/Services/SoYouStart/Ip/SoYouStartIp.php
+++ app/Services/SoYouStart/Ip/SoYouStartIp.php
@@ -2,6 +2,8 @@
 namespace App\Services\SoYouStart\Ip;
 
 use Ovh\Api;
+use PhpIP\IPv4Block;
+use PhpIP\IPv6Block;
 
 /**
  * SoYouStart IP related operations
@@ -22,6 +24,10 @@ use Ovh\Api;
  */
 class SoYouStartIp
 {
+    protected $ovh_api;
+
+    const IP_TYPE_ENUM = ["cdn", "cloud", "dedicated", "failover", "hosted_ssl", "housing", "loadBalancing", "mail", "overthebox", "pcc", "pci", "private", "vpn", "vps", "vrack", "xdsl"];
+
     private static $mapStringToClass = [
         'antihack' => AntiHack::class,
         'arp' => Arp::class,
@@ -47,5 +53,84 @@ class SoYouStartIp
     public function __get($name)
     {
         return array_key_exists($name, self::$mapStringToClass) ? new self::$mapStringToClass[$name]($this->ovh_api) : null;
+    }
+
+    /**
+     * Get all IP available on this user
+     *
+     * @param string $description Search by IP description. Optional.
+     * @param string|IPv4Block|IPv6Block $ip Search by IP address/block. Optional.
+     * @param string $routedToServiceName Search by service name. Optional.
+     * @param string $type Search by IP type. Optional.
+     * @return array A list of IPs the user has
+     */
+    public function all($description = null, $ip = null, $routedToServiceName = null, $type = null) {
+        $query = [];
+
+        if (isset($description)) {
+            $query['description'] = $description;
+        }
+
+        if (isset($ip)) {
+            $query['ip'] = $ip;
+        }
+
+        if (isset($routedToServiceName)) {
+            $query['routedTo.serviceName'] = $routedToServiceName;
+        }
+
+        if (isset($type) && in_array($type, self::IP_TYPE_ENUM)) {
+            $query['type'] = $type;
+        }
+
+        if ($query) {
+            return $this->ovh_api->get('/ip', $query);
+        }
+
+        return $this->ovh_api->get('/ip');
+    }
+
+    /**
+     * Get IP address/block details
+     *
+     * @param string|IPv4Block|IPv6Block $ip IP address/block to query
+     * @return array IP address/block details
+     */
+    public function get($ip) {
+        return $this->ovh_api->get(sprintf('/ip/%s', urlencode($ip)));
+    }
+
+    /**
+     * Update the description of the IP address/block
+     *
+     * @param string|IPv4Block|IPv6Block $ip IP address/block to set description
+     * @param string $description Description to set on the IP address/block
+     * @return void
+     */
+    public function update($ip, $description) {
+        $this->ovh_api->get(sprintf('/ip/%s', urlencode($ip)), ['description' => $description]);
+    }
+
+    /**
+     * Change the RIPE organization ID of the IP address/block
+     *
+     * @param string|IPv4Block|IPv6Block $ip IP address to change RIPE organization ID
+     * @param string $ripeOrg The RIPE organization ID to change to
+     * @return array The task object created to track the status
+     */
+    public function changeOrg($ip, $ripeOrg) {
+        return $this->ovh_api->post(sprintf('/ip/%s/changeOrg', urlencode($ip)), ['organisation' => $ripeOrg]);
+    }
+
+    /**
+     * Terminate the provided IP address/block
+     *
+     * @deprecated Marked as deprecated on SoYouStart API Console
+     * @see Service The service class that will contain the terminate function
+     * @param string|IPv4Block|IPv6Block IP address/block to terminate
+     * @return array The task object created to track the status
+     */
+    public function terminate($ip) {
+        return $this->ovh_api->post(sprintf('/ip/%s/terminate', urlencode($ip)));
     }
 }
