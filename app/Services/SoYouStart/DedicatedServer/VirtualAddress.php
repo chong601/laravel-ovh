59de9c39c0a6185c869bfd3e5cf18b51f245843d
Chong Jin Yi <chong601.13@gmail.com>
2022-07-21 22:32:15 +0800

    Finish virtualMac and virtualAddress service

---
+++ app/Services/SoYouStart/DedicatedServer/VirtualAddress.php
@@ -0,0 +1,30 @@
+<?php
+namespace App\Services\SoYouStart\DedicatedServer;
+
+use Ovh\Api;
+
+class VirtualAddress
+{
+    protected $ovh_api;
+
+    public function __construct(Api $ovh_api)
+    {
+        $this->ovh_api = $ovh_api;
+    }
+
+    public function all($serviceName, $macAddress) {
+        return $this->ovh_api->get(sprintf('/dedicated/server/%s/virtualMac/%s/virtualAddress', $serviceName, $macAddress));
+    }
+
+    public function create($serviceName, $macAddress, $ipAddress, $virtualMachineName) {
+        return $this->ovh_api->post(sprintf('/dedicated/server/%s/virtualMac/%s/virtualAddress', $serviceName), ['macAddress' => $macAddress, 'ipAddress' => $ipAddress, 'virtualMachineName' => $virtualMachineName]);
+    }
+
+    public function delete($serviceName, $macAddress, $ipAddress) {
+        return $this->ovh_api->delete(sprintf('/dedicated/server/%s/virtualMac/%s/virtualAddress/%s', $serviceName, $macAddress, urlencode($ipAddress)));
+    }
+
+    public function get($serviceName, $macAddress, $ipAddress) {
+        return $this->ovh_api->get(sprintf('/dedicated/server/%s/virtualMac/%s/virtualAddress/%s', $serviceName, $macAddress, urlencode($ipAddress)));
+    }
+}
--- app/Services/SoYouStart/DedicatedServer/VirtualMac.php
+++ app/Services/SoYouStart/DedicatedServer/VirtualMac.php
@@ -5,10 +5,33 @@ use Ovh\Api;
 
 class VirtualMac
 {
+    const VMAC_TYPE_ENUM = ["ovh","vmware"];
     protected $ovh_api;
+
+    private static $mapStringToClass = [
+        'virtualAddress' => VirtualAddress::class,
+    ];
 
     public function __construct(Api $ovh_api)
     {
         $this->ovh_api = $ovh_api;
+    }
+
+    // Enable Stripe-like attribute accessing to a specific feature to query
+    public function __get($name)
+    {
+        return array_key_exists($name, self::$mapStringToClass) ? new self::$mapStringToClass[$name]($this->ovh_api) : null;
+    }
+
+    public function all($serviceName) {
+        return $this->ovh_api->get(sprintf('/dedicated/server/%s/virtualMac', $serviceName));
+    }
+
+    public function create($serviceName, $ipAddress, $type, $virtualMachineName) {
+        return $this->ovh_api->post(sprintf('/dedicated/server/%s/virtualMac', $serviceName), ['ipAddress' => $ipAddress, 'type' => $type, 'virtualMachineName' => $virtualMachineName]);
+    }
+
+    public function get($serviceName, $macAddress) {
+        return $this->ovh_api->get(sprintf('/dedicated/server/%s/virtualMac/%s', $serviceName, $macAddress));
     }
 }
