bd1a59bb4f1fef4beecc476b9afcaa3412f2c4a4
Chong Jin Yi <chong601.13@gmail.com>
2022-07-22 22:43:06 +0800

    Finish dedicatedServer Install class

--- app/Services/SoYouStart/DedicatedServer/Install.php
+++ app/Services/SoYouStart/DedicatedServer/Install.php
@@ -11,4 +11,47 @@ class Install
     {
         $this->ovh_api = $ovh_api;
     }
+
+    public function compatibleTemplatePartitionSchemes($serviceName, $templateName) {
+        return $this->ovh_api->get(sprintf('/dedicated/server/%s/install/compatibleTemplatePartitionSchemes', $serviceName), ['templateName' => $templateName]);
+    }
+
+    public function compatibleTemplates($serviceName) {
+        return $this->ovh_api->get(sprintf('/dedicated/server/%s/install/compatibleTemplates', $serviceName));
+    }
+
+    public function hardwareRaidProfile($serviceName) {
+        return $this->ovh_api->get(sprintf('/dedicated/server/%s/install/hardwareRaidProfile', $serviceName));
+    }
+
+    public function hardwareRaidSize($serviceName, $templateName, $partitionSchemeName) {
+        return $this->ovh_api->get(sprintf('/dedicated/server/%s/install/hardwareRaidSize', $serviceName), ['templateName' => $templateName, 'partitionSchemeName' => $partitionSchemeName]);
+    }
+
+    public function start($serviceName, $templateName, $details = null, $partitionSchemeName = null, $userMetadata = null) {
+        $params = [
+            'templateName' => $templateName
+        ];
+        if (isset($details)) {
+            $params['details'] = $details;
+        }
+
+        if (isset($partitionSchemeName)) {
+            $params['partitionSchemeName'] = $partitionSchemeName;
+        }
+
+        if (isset($userMetadata)) {
+            $params['userMetadata'] = $userMetadata;
+        }
+
+        return $this->ovh_api->post(sprintf('/dedicated/server/%s/install/start', $serviceName), $params);
+    }
+
+    public function status($serviceName) {
+        return $this->ovh_api->get(sprintf('/dedicated/server/%s/install/status', $serviceName));
+    }
+
+    public function templateCapabilitues($serviceName, $templateName) {
+        return $this->ovh_api->get(sprintf('/dedicated/server/%s/install/templateCapabilities', $serviceName), ['templateName' => $templateName]);
+    }
 }
