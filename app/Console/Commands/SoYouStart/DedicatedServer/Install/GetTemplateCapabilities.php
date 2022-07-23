bd300ec934ab26168d720704d3fe2b5523b7c9b7
Chong Jin Yi <chong601.13@gmail.com>
2022-07-23 22:24:37 +0800

    Add a few more install commands

---
+++ app/Console/Commands/SoYouStart/DedicatedServer/Install/GetTemplateCapabilities.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Console\Commands\SoYouStart\DedicatedServer\Install;
+
+use App\Services\SoYouStart\SoYouStartService;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\App;
+
+class GetTemplateCapabilities extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'soyoustart:dedicatedserver:install:gettemplatecapabilities {serviceName : Service name} {templateName : Template name}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Get installation template capabilities';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        /** @var \App\Services\SoYouStart\SoYouStartService */
+        $ovh_api = App::makeWith(SoYouStartService::class, [
+            'application_key' => config('soyoustart.application_key'),
+            'application_secret' => config('soyoustart.application_secret'),
+            'endpoint' => 'soyoustart-ca',
+            'consumer_key' => config('soyoustart.consumer_key')
+        ]);
+
+        $serviceName = $this->argument('serviceName');
+        $templateName = $this->argument('templateName');
+
+        $result = $ovh_api->dedicatedServer->install->templateCapabilities($serviceName, $templateName);
+
+        $this->info(json_encode($result, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES));
+        return 0;
+    }
+}
