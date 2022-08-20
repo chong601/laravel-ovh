1506208ded5a4e5a1b6dbb5b3c59587a47be1295
Chong Jin Yi <chong601.13@gmail.com>
2022-08-20 23:33:09 +0800

    HERE COMES THE BEST HACK EVER.

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -11,6 +11,25 @@ use Illuminate\Support\Facades\Route;
 class RouteServiceProvider extends ServiceProvider
 {
     /**
+     * Regular expression matching IPv4 or IPv6 address with subnet
+     *
+     * Reference:
+     * https://regex101.com/library/rB9pG1
+     * https://regex101.com/library/gN7wJ2
+     */
+    public const IP_MATCHING_WITH_SUBNET = '[[:xdigit:]]{1,4}(?::[[:xdigit:]]{1,4}){7}|::|:(?::[[:xdigit:]]{1,4}){1,6}|[[:xdigit:]]{1,4}:(?::[[:xdigit:]]{1,4}){1,5}|(?:[[:xdigit:]]{1,4}:){2}(?::[[:xdigit:]]{1,4}){1,4}|(?:[[:xdigit:]]{1,4}:){3}(?::[[:xdigit:]]{1,4}){1,3}|(?:[[:xdigit:]]{1,4}:){4}(?::[[:xdigit:]]{1,4}){1,2}|(?:[[:xdigit:]]{1,4}:){5}:[[:xdigit:]]{1,4}|(?:[[:xdigit:]]{1,4}:){1,6}:\/[0-9]{0,3}|(([12]?[0-9]{1,2}|2[0-4][0-9]|25[0-5])(\.|\/)){4}[0-9]{0,2}';
+
+    /**
+     * Regular expression matching IPv4 or IPv6 address with subnet
+     *
+     * Reference:
+     * https://regex101.com/library/rB9pG1
+     * https://regex101.com/library/dT0vT3
+     */
+    public const IP_MATCHING_WITHOUT_SUBNET = '([[:xdigit:]]{1,4}(?::[[:xdigit:]]{1,4}){7}|::|:(?::[[:xdigit:]]{1,4}){1,6}|[[:xdigit:]]{1,4}:(?::[[:xdigit:]]{1,4}){1,5}|(?:[[:xdigit:]]{1,4}:){2}(?::[[:xdigit:]]{1,4}){1,4}|(?:[[:xdigit:]]{1,4}:){3}(?::[[:xdigit:]]{1,4}){1,3}|(?:[[:xdigit:]]{1,4}:){4}(?::[[:xdigit:]]{1,4}){1,2}|(?:[[:xdigit:]]{1,4}:){5}:[[:xdigit:]]{1,4}|(?:[[:xdigit:]]{1,4}:){1,6}:)|(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)';
+
+
+    /**
      * The path to the "home" route for your application.
      *
      * This is used by Laravel authentication to redirect users after login.
@@ -47,6 +66,11 @@ class RouteServiceProvider extends ServiceProvider
                 ->namespace($this->namespace)
                 ->group(base_path('routes/web.php'));
         });
+
+        // OVH and their infinite wisdom to use IP ADDRESS on requests.
+        Route::pattern('ipAddress', self::IP_MATCHING_WITHOUT_SUBNET);
+        Route::pattern('ip', self::IP_MATCHING_WITH_SUBNET);
+        Route::pattern('ipReverse', self::IP_MATCHING_WITHOUT_SUBNET);
     }
 
     /**
