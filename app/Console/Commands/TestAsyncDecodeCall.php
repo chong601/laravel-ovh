5b04dba5d30afca89076ec20e89f48270f6d7316
Chong Jin Yi <chong601.13@gmail.com>
2022-09-04 22:25:34 +0800

    Take me out of here. I have been working on this for two days and I still have no idea how to do it properly.

--- app/Console/Commands/TestAsyncDecodeCall.php
+++ app/Console/Commands/TestAsyncDecodeCall.php
@@ -2,14 +2,11 @@
 
 namespace App\Console\Commands;
 
-use App\Services\SoYouStart\SoYouStartService;
-use Carbon\Carbon;
-use GuzzleHttp\Client;
-use GuzzleHttp\Promise\Utils;
-use GuzzleHttp\Psr7\Request;
+use App\Services\PhpOvhNG\PhpOvhAsyncRequest;
+use App\Services\PhpOvhNG\PhpOvhNG;
+use GuzzleHttp\Psr7\Response;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\App;
-use Ovh\Api;
 
 class TestAsyncDecodeCall extends Command
 {
@@ -44,55 +41,129 @@ class TestAsyncDecodeCall extends Command
      */
     public function handle()
     {
-        $client = new Client();
-        $baseUrl = 'https://ca.api.soyoustart.com/1.0';
+        $start = microtime(true);
+        /** @var PhpOvhNG $ovh_api*/
+        $ovh_api = App::makeWith(PhpOvhNG::class, [
+            'application_key' => config('soyoustart.application_key'),
+            'application_secret' => config('soyoustart.application_secret'),
+            'api_endpoint' => 'soyoustart-ca',
+            'consumer_key' => config('soyoustart.consumer_key')
+        ]);
 
-        $requests = [
-            'server' => [
-                'method' => 'GET',
-                'path' => '/dedicated/server',
-                'content' => null
-            ],
-            'ip' => [
-                'method' => 'GET',
-                'path' => '/ip',
-                'content' => null
-            ]
-        ];
-        // Configure headers
-        $headers = [];
-        $contentType = 'application/json; charset=utf-8';
-        $applicationKey = config('soyoustart.application_key');
-        $applicationSecret = config('soyoustart.application_secret');
-        $consumerKey = config('soyoustart.consumer_key');
+        // Get all dedicated server and IPs available
+        $phase1Promises = new PhpOvhAsyncRequest;
+        $phase1Promises['dedicated.server'] = $ovh_api->get('/dedicated/server', null, null, true, true);
+        $phase1Promises['ip'] = $ovh_api->get('/ip', null, null, true, true);
 
-        // Signature will be generated later
-        $promises = [];
-        foreach ($requests as $key => $value) {
-            $now = Carbon::now()->getTimestamp();
-            $headers['X-Ovh-Application'] = $applicationKey;
-            $headers['X-Ovh-Timestamp'] = $now;
-            $headers['X-Ovh-Consumer'] = $consumerKey;
-            $toSign = sprintf('%s+%s+%s+%s+%s+%s',
-                $applicationSecret,
-                $consumerKey,
-                $value['method'],
-                $baseUrl . $value['path'],
-                json_encode($value['content']),
-                // We cheat.
-                $now
-            );
-            $headers['X-Ovh-Signature'] = '$1$' . sha1($toSign);
-            $request = new Request($value['method'], $baseUrl . $value['path'], $headers);
-            $request->getBody()->write(json_encode($value['content']));
-            $promises[$key] = $client->sendAsync($request, ['headers']);
+        $phase1Response = $ovh_api->callAsync($phase1Promises);
+        $this->info(json_encode($phase1Response['dedicated.server']));
+        $dedicatedServer = $phase1Response['dedicated.server'];
+        $ips = $phase1Response['ip'];
+
+        // Get all dedicated server IPs as well as list of available virtual MAC on per-server basis
+        $phase2Promises = new PhpOvhAsyncRequest;
+        foreach ($dedicatedServer as $serviceName) {
+            $phase2Promises[$serviceName] = $ovh_api->get('/dedicated/server/' . $serviceName . '/ips', null, null, true, true);
+            $phase2Promises[$serviceName . '.virtualMac'] = $ovh_api->get('/dedicated/server/' . $serviceName . '/virtualMac', null, null, true, true);
         }
-        $responses = Utils::settle($promises)->wait();
-        print_r($responses);
-        foreach ($responses as $response) {
-//            var_dump($responses);
-            $this->info($response['value']->getBody());
+
+        foreach ($ips as $ip) {
+            $phase2Promises[$ip] = $ovh_api->get('/ip/' . urlencode($ip), null, null, true, true);
         }
+
+        $phase2Responses = $ovh_api->callAsync($phase2Promises);
+
+        $this->info(json_encode($phase2Responses));
+
+        // Query all virtual MAC details as well, list of virtual address on per virtual MAC basis
+        $phase3Promises = new PhpOvhAsyncRequest;
+        $virtualMacs = [];
+        foreach ($dedicatedServer as $serviceName) {
+            // Grab every dedicated server available virtual MAC and start traversing through the MAC list
+            $virtualMacs = $phase2Responses[$serviceName . '.virtualMac'];
+            foreach ($virtualMacs as $virtualMac) {
+                $phase3Promises[$serviceName . '.' . $virtualMac] = $ovh_api->get('/dedicated/server/'. $serviceName . '/virtualMac/' . $virtualMac, null, null, true, true);
+                $phase3Promises[$serviceName . '.' . $virtualMac . '.virtualAddress'] = $ovh_api->get('/dedicated/server/'. $serviceName . '/virtualMac/' . $virtualMac . '/virtualAddress', null, null, true, true);
+            }
+        }
+
+        // Query all available reverse domains on per IP block basis
+        foreach ($ips as $ip) {
+            $phase3Promises['ip.reverse.' . $ip] = $ovh_api->get('/ip/' . urlencode($ip) . '/reverse', null, null, true, true);
+        }
+
+        $phase3Responses = $ovh_api->callAsync($phase3Promises);
+        $this->info(json_encode(array_keys($phase3Responses)));
+        $phase4Promises = new PhpOvhAsyncRequest;
+        $virtualMacArray = [];
+        $virtualAddressArray = [];
+        foreach ($dedicatedServer as $serviceName) {
+            foreach (array_keys($phase3Responses) as $responseKey)
+            {
+                if (strpos($responseKey, $serviceName) === 0) {
+                    if (strpos($responseKey, '.virtualAddress')) {
+                        $virtualMacIps = $phase3Responses[$responseKey];
+                        foreach ($virtualMacIps as $virtualMacIp) {
+                            if (!$phase4Promises->offsetExists($virtualMacIp)) {
+                                $macAddress = str_replace('.virtualAddress', '', str_replace($serviceName . '.', '', $responseKey));
+                                $phase4Promises[$virtualMacIp] = $ovh_api->get('/dedicated/server/' . $serviceName . '/virtualMac/' . $macAddress . '/virtualAddress/' . urlencode($virtualMacIp), null, null, true, true);
+                            }
+                        }
+                    } else {
+                        $macAddress = str_replace($serviceName . '.', '', $responseKey);
+                        $virtualMacArray[$macAddress] = $phase3Responses[$responseKey];
+                    }
+                }
+            }
+//            foreach ($virtualMacs as $virtualMac) {
+//                $virtualAddresses = $phase3Responses[$serviceName . '.' . $virtualMac . '.virtualAddress'];
+//                foreach ($virtualAddresses as $virtualAddress) {
+//                    if (!$phase4Promises->offsetExists($serviceName. '.' . $virtualMac . '.virtualAddress' . $virtualAddress, $virtualAddresses)) {
+//                        $phase4Promises[$serviceName. '.' . $virtualMac . '.virtualAddress' . $virtualAddress . '.' . $virtualAddresses] = $ovh_api->get('/dedicated/server/' . $serviceName . '/virtualMac', null, null, true, true);
+//                    }
+//                }
+//            }
+        }
+        $phase4Responses = $ovh_api->callAsync($phase4Promises);
+        $this->info(json_encode($phase4Responses));
+
+//        $this->info(json_encode($phase3Responses));
+//        $phase4Promises = new PhpOvhAsyncRequest;
+//        foreach ($virt)
+//
+//
+//        $phase4Responses = $ovh_api->callAsync($phase4Promises);
+//        // Get virtual MAC available on per dedicated server basis
+//
+//        $virtualMacArray = [];
+//        foreach ($dedicatedServer as $serviceName) {
+//            foreach ($virtualMacs as $virtualMac) {
+//                $virtualMacArray[$virtualMac] = $phase3Responses[$serviceName . '.' . $virtualMac];
+//                $virtualMacArray[$virtualMac]['address'] = $phase3Responses[$serviceName . '.' . $virtualMac . '.virtualAddress'];
+//            }
+//        }
+//
+//        $rev
+//        foreach ($ips as $ip) {
+//
+//        }
+//
+//        $phase5Promises = new PhpOvhAsyncRequest;
+//        foreach ($virtualMacs as $virtualMac) {
+////            $phase5Promises[$virtualMac] = $phaz
+//        }
+//        $this->info(json_encode($phase4Responses, JSON_PRETTY_PRINT));
+//
+////        $virtualMacArray = [];
+////        foreach ($virtualMacs as $virtualMac) {
+////            $virtualMacArray[$virtualMac] = [
+////                'detail' => $phase4Responses[$virtualMac],
+////                'reverse' => []
+////            ];
+////        }
+
+        $end = microtime(true);
+        $this->info($end - $start);
         return 0;
     }
 }
